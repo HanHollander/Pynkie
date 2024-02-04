421e7f538591732ae7024d10e6e4ca339a2d7162
Han Hollander <han_hollander@hotmail.nl>
2024-02-04 17:13:33 +0100

    add setup to Pynkie init

--- src/pynkie/run.py
+++ src/pynkie/run.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-from typing import NoReturn
 import pygame as pg
 from time import time
 
@@ -9,31 +7,34 @@ import pynkie.setup as setup
 import pynkie.debug as debug
 from pynkie.view import View
 
-@dataclass()
 class Pynkie:
-    views: list[View]
-    models: list[Model]
-    event_listenters: dict[int, list[events.EventListener]]
 
-    screen_width: int = 1920
-    screen_height: int = 1080
-    max_framerate: int = 60
-    debug_info: bool = False
-    use_default_cursor: bool = True
+    def __init__(self, views: list[View], models: list[Model], event_listenters: dict[int, list[events.EventListener]],
+                 screen_width: int, screen_height: int, max_framerate: int, debug_info: bool, use_default_cursor: bool) -> None:
+        
+        self.views: list[View] = views
+        self.models: list[Model] = models
+        self.event_listenters: dict[int, list[events.EventListener]] = event_listenters
 
-    def run(self) -> None:
+        self.screen_width: int = screen_width
+        self.screen_height: int = screen_height
+        self.max_framerate: int = max_framerate
+        self.debug_info: bool = debug_info
+        self.use_default_cursor: bool = use_default_cursor
+
         # setup mouse
         pg.mouse.set_visible(self.use_default_cursor)
 
         # setup screen and clock
-        display = setup.setup_screen(self.screen_width, self.screen_height)
-        clock = pg.time.Clock()
+        self.display = setup.setup_screen(self.screen_width, self.screen_height)
+        self.clock = pg.time.Clock()
 
+    def run(self) -> None:
         # run main loop
-        self.main_loop(display, clock)
+        self.main_loop()
 
 
-    def main_loop(self, display: pg.Surface, clock: pg.time.Clock) -> NoReturn:
+    def main_loop(self, ) -> None:
         prev_time: float = time()
         dt: float = 0.  # delta time [s]
         while True:
@@ -47,16 +48,16 @@ class Pynkie:
 
             # draw elements
             for view in self.views:
-                view.draw(display)
+                view.draw(self.display)
 
-            # debug info
-            if self.debug_info: debug.display_debug(display)
+            # debug info, blit on top of everything
+            if self.debug_info: debug.display_debug(self.display)
             
             # update screen
             pg.display.flip()
 
             # tick
-            clock.tick(self.max_framerate)
+            self.clock.tick(self.max_framerate)
             
             # delta time
             now: float = time()
